19671af9275b01aa98229286a839896391b1a6f5
maicol604 <maicol.fario@gmail.com>
2021-11-17 15:32:48 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import Flow from './Components/Flow';
+
+const sampleAnswers = [
+  {
+    text:'Pregunta 1'
+  },
+  {
+    text:'Pregunta 2'
+  },
+  {
+    text:'Pregunta 3'
+  },
+  {
+    text:'pregunta 4'
+  },
+]
+
+const initialElements = [
+  {
+    id: '1',
+    type: 'normal',
+    position: { x: 100, y: 100 },
+    data: { title:'Titulo de la pregunta', id:1, answers:[...sampleAnswers] },
+    dragHandle: '.drag-handle'
+  },  
+  {
+    id: '2',
+    type: 'normal',
+    position: { x: 100, y: 100 },
+    data: { title:'Titulo de la pregunta', id:2, answers:[] },
+    dragHandle: '.drag-handle'
+  }, 
+  {
+    id: '3',
+    type: 'normal',
+    position: { x: 100, y: 100 },
+    data: { title:'Titulo de la pregunta', id:3, answers:[] },
+    dragHandle: '.drag-handle'
+  }, 
+  {
+    id: '4',
+    type: 'normal',
+    position: { x: 100, y: 100 },
+    data: { title:'Titulo de la pregunta', id:4, answers:[] },
+    dragHandle: '.drag-handle'
+  }, 
+  {
+    id: '5',
+    type: 'normal',
+    position: { x: 100, y: 100 },
+    data: { title:'Titulo de la pregunta', id:5, answers:[] },   
+    dragHandle: '.drag-handle' 
+  },   
+  {
+    id: 'edges-e3-5',
+    source: '4',
+    target: '5',
+    label: 'Yes',
+    animated: true,
+    style: { stroke: '#15295c', strokeWidth:3 },
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flow
+        initialElements={initialElements}
+      />
     </div>
   );
 }
---
+++ src/Components/Flow/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Edge from './Edge';
+import { NormalNode } from './Node';
+import ReactFlow, { removeElements, addEdge } from 'react-flow-renderer';
+
+const nodeTypes = {
+  normal: NormalNode,
+};
+
+const edgeTypes = {
+  custom: Edge,
+};
+
+export default ({ initialElements=[] }) => {
+
+  const [elements, setElements] = React.useState(initialElements);
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => {
+    setElements((els) => addEdge({
+      ...params, 
+      animated: true,
+      style: { 
+        stroke: '#15295c', 
+        strokeWidth:3 
+      },
+    }
+    , els));
+    console.log(params)
+  }
+
+  React.useEffect(()=>{
+    setElements(initialElements);
+  },[initialElements])
+
+  return (
+    <div style={{ height: '100vh', backgroundColor: '#f0f0f0' }}>
+
+      <ReactFlow
+        elements={elements}
+        onElementsRemove={onElementsRemove}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}      
+        edgeTypes={edgeTypes} 
+        onElementClick={(e)=>{
+          console.log('event handler',Object.keys(e), e.type==='click')
+          if(e.type==='click')
+            alert('Click')
+        }}
+      />
+
+    </div>
+  )
+}
